e5b53b61930f35768f97aa8f4ea8b2938278b02f
Jessica Latorre Cabral <jessica.latorre182@gmail.com>
2021-08-17 21:32:02 -0300

    Correções e ajustes no código

--- index.js
+++ index.js
@@ -2,56 +2,62 @@
 const listaDeAlimentos = ['arroz', 'feijão', 'miojo', 'espaguete', 'milho', 'biscoito', 'banana', 'maçã', 'uva', 'espinafre', 'brócolis', 'tomate']
 const listaDeBrinquedos = ['Hot Wheels', 'massinha de modelar', 'Barbie', 'Boneca Baby Alive', 'Quebra-Cabeça 3D Módulo Lunar Apollo', 'Bonecos Toy Story', 'BeyBlade', 'Lego' ]
 
-let vitrine = prompt('Qual tipo de produto deseja ver? alimentos ou brinquedos')
-
- 
-
+alert("Bem-vindo(a) a lojinha no console! \nPrimeiro abra o console do navegador e em seguida clique em 'começar a comprar' para fazer suas compras. \nLembre-se: para sair da loja digite 'sair' \nBoas compras!!")
+let vitrine
 
 
-    
-while(vitrine != 'sair'){
 
-    if(vitrine === 'alimentos'){
-        for (let index = 0; index < listaDeAlimentos.length; index++) {
-            const element = listaDeAlimentos[index]
-            console.log(element)
+
+function chamarLoop() {
+
+
+    while(vitrine != 'sair') {
+        
+    vitrine = prompt('Qual tipo de produto deseja ver? alimentos ou brinquedos')
+
+        if(vitrine === 'alimentos') {
+
+            for (let index = 0; index < listaDeAlimentos.length; index++) {
+                const element = listaDeAlimentos[index]
+                console.log(element)
+            }
             
+            produto = prompt('Qual produto deseja comprar?')
+            const indexProduto = listaDeAlimentos.indexOf(produto)
+
+            console.log(`Compra realizada com sucesso. Você comprou ${produto}!`)
+            listaDeAlimentos.splice(indexProduto,1)
+
+            console.log(`Alimentos disponíveis: ${listaDeAlimentos}`)
         }
         
-       
-         produto = prompt('Qual produto deseja comprar?')
-         
-     
-         const indexProduto = listaDeAlimentos.indexOf(produto)
-         listaDeAlimentos.splice(indexProduto,1)
-         console.log('compra realizada com sucesso!')
-         console.log(listaDeAlimentos)
- 
-         
-     
-     }
-vitrine = prompt('Qual tipo de produto deseja ver? alimentos ou brinquedos')
-if (vitrine === 'brinquedos') {
-         for (let index = 0; index < listaDeBrinquedos.length; index++) {
-             const element = listaDeBrinquedos[index]
-             console.log(element)
-             
-         }
-        
-          produto = prompt('Qual produto deseja comprar?')
-          
-     
-         indexProduto = listaDeBrinquedos.indexOf(produto)
-         listaDeBrinquedos.splice(indexProduto,1)
-         console.log('compra realizada com sucesso!')
-         console.log(listaDeBrinquedos)
+        if (vitrine === 'brinquedos') {
 
-     
-     }
+            for (let index = 0; index < listaDeBrinquedos.length; index++) {
+                const element = listaDeBrinquedos[index]
+                console.log(element)
+            }
+            
+            produto = prompt('Qual produto deseja comprar?')
+            indexProduto = listaDeBrinquedos.indexOf(produto)
+
+            console.log(`Compra realizada com sucesso. Você comprou ${produto}!`)
+            listaDeBrinquedos.splice(indexProduto,1)
+
+            console.log(`Brinquedos disponíveis na loja: ${listaDeBrinquedos}`)
+        }
+    }
+
+    if(vitrine === 'sair'){
+        console.log('Obrigada por comprar aqui! Até mais')
+    }
 
 }
 
- 
+    
+    
+
+    
 
 
 
